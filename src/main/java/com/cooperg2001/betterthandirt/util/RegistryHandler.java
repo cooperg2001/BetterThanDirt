c7839f4e8d2fef09669e1d6f9269c88718ec6e3a
cooperg2001 <stephenkoebel@gmail.com>
2020-05-18 00:52:18 -0400

    Implemented stone and cobblestone armor
    Added textures, models, and item recipes for stone and cobblestone armor. Armor effects to come later

--- src/main/java/com/cooperg2001/betterthandirt/util/RegistryHandler.java
+++ src/main/java/com/cooperg2001/betterthandirt/util/RegistryHandler.java
@@ -1,9 +1,10 @@
 package com.cooperg2001.betterthandirt.util;
 
 import com.cooperg2001.betterthandirt.BetterThanDirt;
-import com.cooperg2001.betterthandirt.items.ArmorBase;
-import com.cooperg2001.betterthandirt.items.bakeddirtarmor.*;
-import com.cooperg2001.betterthandirt.items.dirtarmor.*;
+import com.cooperg2001.betterthandirt.items.baked_dirt_armor;
+import com.cooperg2001.betterthandirt.items.cobblestone_armor;
+import com.cooperg2001.betterthandirt.items.dirt_armor;
+import com.cooperg2001.betterthandirt.items.stone_armor;
 import com.cooperg2001.betterthandirt.util.CustomArmorMaterials.CustArmorMaterial;
 
 import net.minecraft.block.Block;
@@ -26,7 +27,7 @@ public class RegistryHandler {
 
     //Items
 
-    public static final RegistryObject<Item> TEST_CHEST = ITEMS.register("test_chest", () -> new ArmorBase(CustArmorMaterial.DIRT_ARMOR, EquipmentSlotType.CHEST, (new Item.Properties()).group(BetterThanDirt.TAB)));
+    //public static final RegistryObject<Item> TEST_CHEST = ITEMS.register("test_chest", () -> new ArmorBase(CustArmorMaterial.DIRT_ARMOR, EquipmentSlotType.CHEST, (new Item.Properties()).group(BetterThanDirt.TAB)));
 
     //NOTE: Clean these up to use a item type for DIRT and BAKED_DIRT instead of a seperate class for each armor
     public static final RegistryObject<Item> DIRT_CHESTPLATE = ITEMS.register("dirt_chestplate", () -> new dirt_armor(CustArmorMaterial.DIRT_ARMOR, EquipmentSlotType.CHEST, (new Item.Properties()).group(BetterThanDirt.TAB)));
@@ -39,5 +40,13 @@ public class RegistryHandler {
     public static final RegistryObject<Item> BAKED_DIRT_LEGGINGS = ITEMS.register("baked_dirt_leggings", () -> new baked_dirt_armor(CustArmorMaterial.BAKED_DIRT_ARMOR, EquipmentSlotType.LEGS, (new Item.Properties()).group(BetterThanDirt.TAB)));
     public static final RegistryObject<Item> BAKED_DIRT_BOOTS = ITEMS.register("baked_dirt_boots", () -> new baked_dirt_armor(CustArmorMaterial.BAKED_DIRT_ARMOR, EquipmentSlotType.FEET, (new Item.Properties()).group(BetterThanDirt.TAB)));
 
+    public static final RegistryObject<Item> COBBLESTONE_CHESTPLATE = ITEMS.register("cobblestone_chestplate", () -> new cobblestone_armor(CustArmorMaterial.COBBLESTONE_ARMOR, EquipmentSlotType.CHEST, (new Item.Properties()).group(BetterThanDirt.TAB)));
+    public static final RegistryObject<Item> COBBLESTONE_HELMET = ITEMS.register("cobblestone_helmet", () -> new cobblestone_armor(CustArmorMaterial.COBBLESTONE_ARMOR, EquipmentSlotType.HEAD, (new Item.Properties()).group(BetterThanDirt.TAB)));
+    public static final RegistryObject<Item> COBBLESTONE_LEGGINGS = ITEMS.register("cobblestone_leggings", () -> new cobblestone_armor(CustArmorMaterial.COBBLESTONE_ARMOR, EquipmentSlotType.LEGS, (new Item.Properties()).group(BetterThanDirt.TAB)));
+    public static final RegistryObject<Item> COBBLESTONE_BOOTS = ITEMS.register("cobblestone_boots", () -> new cobblestone_armor(CustArmorMaterial.COBBLESTONE_ARMOR, EquipmentSlotType.FEET, (new Item.Properties()).group(BetterThanDirt.TAB)));
 
+    public static final RegistryObject<Item> STONE_CHESTPLATE = ITEMS.register("stone_chestplate", () -> new stone_armor(CustArmorMaterial.STONE_ARMOR, EquipmentSlotType.CHEST, (new Item.Properties()).group(BetterThanDirt.TAB)));
+    public static final RegistryObject<Item> STONE_HELMET = ITEMS.register("stone_helmet", () -> new stone_armor(CustArmorMaterial.STONE_ARMOR, EquipmentSlotType.HEAD, (new Item.Properties()).group(BetterThanDirt.TAB)));
+    public static final RegistryObject<Item> STONE_LEGGINGS = ITEMS.register("stone_leggings", () -> new stone_armor(CustArmorMaterial.STONE_ARMOR, EquipmentSlotType.LEGS, (new Item.Properties()).group(BetterThanDirt.TAB)));
+    public static final RegistryObject<Item> STONE_BOOTS = ITEMS.register("stone_boots", () -> new stone_armor(CustArmorMaterial.STONE_ARMOR, EquipmentSlotType.FEET, (new Item.Properties()).group(BetterThanDirt.TAB)));
 }
